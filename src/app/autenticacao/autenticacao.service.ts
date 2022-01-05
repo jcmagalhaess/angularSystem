0674bda36c7fdef144bc8a8c5ebaba045b3054b9
Julio Magalhaes <julio.magalhaes@maps.com.br>
2022-01-05 12:56:59 -0300

    Creating Authentication

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -1,9 +1,29 @@
+import { UsuarioService } from './usuario/usuario.service';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+const API = environment.urlAPI;
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutenticacaoService {
+  constructor(
+    private _httpClient: HttpClient,
+    private _usuarioService: UsuarioService
+  ) {}
 
-  constructor() { }
+  autenticar(email: string, password: string): Observable<HttpResponse<any>> {
+    return this._httpClient.post(`${API}/user/login`,
+    {
+      email: email,
+      password: password,
+    },
+    {
+      observe: 'response',
+    }
+  )
+  }
 }
